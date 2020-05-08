03b4ba58bd33eeba26afa01671b12d0dd88adc58
Arthur <arthurv@mail.tau.ac.il>
2020-05-08 23:04:34 +0300

    Initial commit - adding IPC(message queue, depends on boost)

--- icy_tower_13_trainer/IcyTowerConsts.h
+++ icy_tower_13_trainer/IcyTowerConsts.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>
 
+#define MSGQUEUE_NAME "ICYTOWER13TRAINER"
 
 // this should be at the base of the IcyTower exe address
 typedef struct _IcyTower13Module{
@@ -22,3 +23,42 @@ typedef struct _Entity {
 	DWORD best_combo;
 
 } GameEntity, *pGameEntity;
+
+//////////////////////////////////////////////////////////////////////////////////
+
+enum IcyTowerMessageType {
+	INVALID_MSG_TYPE,
+	ChangeEnabledFeature,	//enable specific feature
+	ChangeValueFeature,		//change value of a feature
+	ChangeEnableAll			//enable\disable trainer
+};
+
+// Available trainer options
+enum IcyTowerFeature {
+	INVALID_FEATURE,
+	FreezeFloor,			//freeze last floor stepped on
+	FreezeCurrentCombo,		//freeze current combo
+	BestCombo,				//set best combo
+	AdditionalScore			//set additional score
+};
+
+class IcyTowerMessage {
+public:
+	IcyTowerMessage(enum IcyTowerMessageType msgtype_, enum IcyTowerFeature ftr_, DWORD value_):_msgtype(msgtype_), _ftr(ftr_), _value(value_) {};
+	IcyTowerMessage() : _msgtype(INVALID_MSG_TYPE), _ftr(INVALID_FEATURE), _value(0) {};
+	enum IcyTowerMessageType _msgtype;
+	enum IcyTowerFeature _ftr;
+	DWORD _value; //if we need to pass value. for booleans - 0 is false, everything else is true
+};
+
+//This defines last received settings from the pipe 
+class Settings{
+public:
+	Settings() :_is_running(true), _should_freeze_combo(false), _should_freeze_floor(false), _frozen_floor(0), _frozen_combo(0){};
+
+	bool _is_running;			//is trainer running
+	bool _should_freeze_floor;	
+	bool _should_freeze_combo;
+	DWORD _frozen_floor;
+	DWORD _frozen_combo;
+};
--- icy_tower_13_trainer/dllmain.cpp
+++ icy_tower_13_trainer/dllmain.cpp
@@ -3,24 +3,91 @@
 #include "IcyTowerConsts.h"
 #include <ostream>
 #include <thread>
-volatile static bool should_break = false;
+#include <boost/interprocess/ipc/message_queue.hpp>
+
+using namespace boost::interprocess;
+volatile static bool dll_should_detach = false;
+
+//only print if in debug mode
+void my_output_debug(const char* format, ...) {
+#ifdef _DEBUG
+	static char buffer[1024];
+	va_list arglist;
+	va_start(arglist, format);
+	vsprintf_s(buffer, sizeof(buffer), format, arglist);
+	va_end(arglist);
+	OutputDebugStringA(buffer);
+#endif
+}
 
 DWORD WINAPI  mainloop(LPVOID param)
 {
-	char debug_message[1024];
+	Settings settings;
+	//Dummy settings for now:
+	settings._should_freeze_floor = true;
+	settings._frozen_floor = 411;
+	IcyTowerMessage cur_message;
+	size_t recv_size = 0;
+	unsigned int priority = 0;
 	// first we need to calculate address of GameEntity
 	pIcyTowerModule base = (pIcyTowerModule) GetModuleHandle(NULL);
 	pGameEntity game = reinterpret_cast<pGameEntity>( *(DWORD*)((long)base->addr_multiplier * 4 + (long)&base->addr));
-#ifdef _DEBUG 
-	snprintf(debug_message, sizeof(debug_message), "icy_tower_trainer: Base addr is %p , GameEntity addr is %p, points to %d\n", base, game, *(DWORD*)game);
-	OutputDebugStringA(debug_message);
-	snprintf(debug_message, sizeof(debug_message), "icy_tower_trainer: Calculated address of last_floor: %p", &game->last_floor);
-	OutputDebugStringA(debug_message);
-#endif
-	while (!should_break)
+	my_output_debug("icy_tower_trainer: Base addr is %p , GameEntity addr is %p, points to %d\n", base, game, *(DWORD*)game);
+	my_output_debug("icy_tower_trainer: Calculated address of last_floor: %p\n", &game->last_floor);
+
+	//Create a message_queue.
+	message_queue::remove(MSGQUEUE_NAME);
+	message_queue mq
+	(	create_only					//only create
+		, MSGQUEUE_NAME				//name
+		, 100                       //max message number
+		, sizeof(IcyTowerMessage)	//max message size
+	);
+
+	while (!dll_should_detach)
 	{
-		//freeze floor
-		game->last_floor = 4242;
+		//check if received a new message
+		if (mq.try_receive(&cur_message, sizeof(cur_message), recv_size, priority)) {
+			if (recv_size != sizeof(cur_message)) {
+				my_output_debug("icy_tower_trainer: Received incorrect size: %d instead of %d", recv_size, sizeof(cur_message));
+			}
+			else {
+				bool should_enable;
+				switch (cur_message._msgtype) {
+				case INVALID_MSG_TYPE:
+					my_output_debug("icy_tower_trainer: Received invalid message!");
+					break;
+				case ChangeEnabledFeature:
+					should_enable = cur_message._value ? false : true;
+					switch (cur_message._ftr) {
+					case INVALID_FEATURE:
+						my_output_debug("icy_tower_trainer: Received invalid feature!");
+					case FreezeFloor:
+						settings._should_freeze_floor = should_enable;
+						break;
+					case FreezeCurrentCombo:
+						settings._should_freeze_combo = should_enable;
+					case BestCombo:
+					case AdditionalScore:
+						break;
+					}
+				case ChangeValueFeature:
+				case ChangeEnableAll:
+					break;
+
+
+				}
+			}
+		}
+		if (settings._is_running) {
+			//check freeze values
+			if (settings._should_freeze_combo) {
+				game->current_combo = settings._frozen_combo;
+			}
+			if (settings._should_freeze_floor) {
+				game->last_floor = settings._frozen_floor;
+			}
+		}
 		Sleep(20);
 	}
 	return 0;
@@ -34,19 +101,15 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-#ifdef _DEBUG
-		OutputDebugStringA("icy_tower_trainer: Received message DLL_PROCESS_ATTACH");
-#endif
+		my_output_debug("icy_tower_trainer: Received message DLL_PROCESS_ATTACH\n");
 		CreateThread(0, 0, mainloop, NULL, 0, NULL);
 		break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
 		break;
     case DLL_PROCESS_DETACH:
-#ifdef _DEBUG
-		OutputDebugStringA("icy_tower_trainer: Received message DLL_PROCESS_DETACH");
-#endif
-		should_break = true;
+		my_output_debug("icy_tower_trainer: Received message DLL_PROCESS_DETACH\n");
+		dll_should_detach = true;
         break;
     }
     return TRUE;
